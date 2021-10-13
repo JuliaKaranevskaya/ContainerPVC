2fe0b26ab0102754ccbef9220ad7bf0f8ba9a67e
Юлия <yuliya.karanevskaya@gmail.com>
2021-10-13 16:35:05 +0300

    commit

--- ContainerPVC/StoryboardPageViewController.swift
+++ ContainerPVC/StoryboardPageViewController.swift
@@ -58,14 +58,14 @@ extension StoryboardPageViewController {
     func goToNextPage() {
        guard let currentViewController = self.viewControllers?.first else { return }
        guard let nextViewController = dataSource?.pageViewController( self, viewControllerAfter: currentViewController ) else { return }
-       setViewControllers([nextViewController], direction: .forward, animated: false, completion: nil)
+       setViewControllers([nextViewController], direction: .forward, animated: true, completion: nil)
        
     }
 
     func goToPreviousPage() {
        guard let currentViewController = self.viewControllers?.first else { return }
        guard let previousViewController = dataSource?.pageViewController( self, viewControllerBefore: currentViewController ) else { return }
-       setViewControllers([previousViewController], direction: .reverse, animated: false, completion: nil)
+       setViewControllers([previousViewController], direction: .reverse, animated: true, completion: nil)
     }
 
 }
